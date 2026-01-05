e9ee4de88b219225597af9d98fad8ffbf8c17047
Diego Martin Marmol <diegomartinmarmol@gmail.com>
2026-01-04 21:12:19 -0300

    feat: add global variable APP_NAME

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  cacheComponents: true,
+    /* config options here */
+    reactStrictMode: true,
+    cacheComponents: true,
+    env: {
+        APP_NAME: 'Demo React App',
+    },
 };
 
 export default nextConfig;
--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,21 +1,21 @@
-'use client'
+'use client';
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { useEffect } from 'react'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Navbar() {
-    const pathname = usePathname()
-    
+    const pathname = usePathname();
+
     useEffect(() => {
-        console.log('Current pathname:', pathname)
-    }, [pathname])
+        console.log('Current pathname:', pathname);
+    }, [pathname]);
 
     const links = [
         { href: '/', label: 'Home', enabled: true },
         { href: '/players', label: 'Players', enabled: true },
         { href: '/author', label: 'Author', enabled: false },
-    ]
+    ];
 
     return (
         <nav className="flex h-full min-h-screen flex-col bg-white py-10 shadow-md">
@@ -25,12 +25,14 @@ export default function Navbar() {
                     alt="Logo"
                     className="h-16 w-16 rounded"
                 />
-                <div className="text-lg font-semibold text-gray-800">Dashboard</div>
+                <div className="text-lg font-semibold text-gray-800">
+                    {process.env.APP_NAME}
+                </div>
             </div>
 
             <div className="flex flex-col gap-4 text-base font-medium text-gray-700 ml-6">
                 {links.map((link) => {
-                    const isActive = pathname === link.href
+                    const isActive = pathname === link.href;
 
                     return (
                         <Link
@@ -49,9 +51,9 @@ export default function Navbar() {
                         >
                             {link.label}
                         </Link>
-                    )
+                    );
                 })}
             </div>
         </nav>
-    )
+    );
 }
